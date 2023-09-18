645ab07adb5e4715885d27af7af5a1e1e615d75f
Morteza <m.rezayi69@gmail.com>
2023-09-18 22:06:50 +0330

    fix some problems

--- tests/scheduler-customizer-test/src/main/kotlin/HelloController.kt
+++ tests/scheduler-customizer-test/src/main/kotlin/HelloController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
     @RequestMapping("/")
-    fun index(): String? {
+    fun index(): String {
         return "Greetings from Spring Boot!"
     }
 }
